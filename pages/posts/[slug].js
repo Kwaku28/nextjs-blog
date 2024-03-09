b08ee227b710957cfc7c871d7e8c6e95cd0182c7
Lawrence Amoafo <rekenburg777@gmail.com>
2024-03-09 15:03:15 +0000

    Implement `getStaticProps` and `getStaticPaths` to prefetch data and match to the unique file path

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,35 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function PostDetailPage() {
+function PostDetailPage(props) {
   return (
-    <PostContent />
+    <PostContent post={props.post} />
   )
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 1800
+  }
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map(slug => ({ params: { slug: slug } })),
+    fallback: false
+  }
 }
 
 export default PostDetailPage;
